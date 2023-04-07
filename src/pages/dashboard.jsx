de60d7a8b58f204d11c2e12c3dcf014ccd1d061f
lucas <lucas.lvn97439@gmail.com>
2023-04-07 15:51:26 +0400

    Page dashboard splitter en 2 parti une pour la map et l'autre pour les graphes

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,9 +1,16 @@
 import { onMount } from "solid-js"
+import MapSection from "../components/MapSection"
 
 export default function Dashboard(){
 
 
     return (<>
-        <div>Page Dashboard</div>
+    <div className="flex flex-wrap w-full h-[100vh]">
+        <section className="w-full lg:w-1/2 bg-red-400">
+            <MapSection />
+        </section>
+
+        <section className="w-full lg:w-1/2 bg-blue-400">frefr</section>
+    </div>
     </>)
 }
