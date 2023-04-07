aecda40571b946f7fe594e9c19951d98787fb14e
lucas <lucas.lvn97439@gmail.com>
2023-04-07 15:50:44 +0400

    Page login retirer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
-import { Switch, Match } from 'solid-js';
+import { Switch, Match, onMount } from 'solid-js';
 import Dashboard from './pages/dashboard';
-import Login from './pages/login';
+import { request } from './request';
+import {map} from './signals'
+
+import Navbar from './components/Navbar';
 
 function App() {
 
+  const updateTitle = () => {
+    map().update({
+      title: {
+        text: 'test'
+      }
+    })
+  }
   // Récuperer le dernier elt de l'url
   const page = window.location.pathname
 
+
   return (<>
+    <Navbar />
     <Switch fallback={<div>Page not found</div>}>
       <Match when={ page == "/" }> <Dashboard/></Match>
-      <Match when={ page == "/login" }> <Login/></Match>
     </Switch>
   </>
   );
