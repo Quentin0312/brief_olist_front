b1edb9f1063eadcf22c9e34576a4e78068a0899a
lucas <lucas.lvn97439@gmail.com>
2023-04-09 11:46:37 +0400

    Ajout d'une navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar(){
     return(<>
         <nav class="flex w-full justify-center py-5 bg-[#2C2C2C]">
-            <h1 class="text-4xl">Olist</h1>
+            <h1 class="text-4xl text-white">Olist</h1>
         </nav>
     </>)
 }
