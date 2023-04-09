c179cd3958d93e2eb349cc20baa626b76904a1b1
lucas <lucas.lvn97439@gmail.com>
2023-04-09 11:46:21 +0400

    Integration des onglets et de leurs bare de navigation au dashboard

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,16 +1,46 @@
-import { onMount } from "solid-js"
+import { Show, onMount } from "solid-js"
 import MapSection from "../components/MapSection"
+import NavigationLabel from "../components/NavigationLabel"
+import { onLabel } from "../signals"
+import Evolution from "../components/evolution"
+import Annonce from "../components/annonce"
+import NVClientRebond from "../components/NVClientRebond"
+import LocalInter from "../components/LocalInter"
 
 export default function Dashboard(){
 
+    const loadTop10 = async () => {
+
+    }
 
     return (<>
     <div className="flex flex-wrap w-full h-[100vh]">
-        <section className="w-full lg:w-1/2 bg-red-400">
+        <section className="w-full lg:w-1/2">
             <MapSection />
         </section>
 
-        <section className="w-full lg:w-1/2 bg-blue-400">frefr</section>
+        <main className="w-full lg:w-1/2 px-10">
+            <NavigationLabel />
+
+            <div className="flex">
+                <Show when={onLabel() == 'evolution'}>
+                    <Evolution />
+                </Show>
+
+
+                <Show when={onLabel() == 'annonce'}>
+                    <Annonce />
+                </Show>
+
+                <Show when={onLabel() == 'nv-client-rebond'}>
+                    <NVClientRebond />
+                </Show>
+
+                <Show when={onLabel() == 'local-inter'}>
+                    <LocalInter />
+                </Show>
+            </div>
+        </main>
     </div>
     </>)
 }
