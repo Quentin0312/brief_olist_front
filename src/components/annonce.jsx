0f1fc842496a0eb65d77c2509e39466866c76100
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-04-19 22:04:45 +0400

    ajout graph description dans l'onglet annonce

--- src/components/annonce.jsx
+++ src/components/annonce.jsx
@@ -1,4 +1,35 @@
+import { createResource, onMount } from "solid-js"
+
+const descriptionChartRequest = async () => (await fetch('http://localhost:8000/api/annonces/', {method: 'GET'})).json().then(
+    response => {loadChart(response.description.labels, response.description.values)})
+
+function loadChart(labels, values){
+    const ctx = document.getElementById('descriptionChart');
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels : labels,
+            datasets : [{
+                label: 'test',
+                data: values,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Volume de ventes selon la longueur de la description des produits'
+                }
+            }
+        } 
+    })
+}
+
+
 export default function Annonce(props){
+    const [descriptionChartData] = createResource(descriptionChartRequest)
+
 
     return(<>
         <main id='lbl-1' class=" flex flex-wrap w-full rounded p-6 bg-[#383838] text-white">
@@ -17,7 +48,7 @@ export default function Annonce(props){
             </section>
 
             <section className="w-full flex flex-wrap">
-                line section
+                <canvas id="descriptionChart"> </canvas>
             </section>
         </main>
     </>)
