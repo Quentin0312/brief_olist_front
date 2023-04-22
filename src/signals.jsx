32cc22de2c0e15ac10f1ab98cf922cb50a7a2e93
lucas.lvn <lucas.lvn97439@gmail.com>
2023-04-22 07:42:22 +0200

    création des signaux RegionFilter & DateFilter

--- src/signals.jsx
+++ src/signals.jsx
@@ -2,3 +2,6 @@ import { createSignal } from "solid-js";
 
 export const [map, setMap] = createSignal()
 export const [onLabel, setOnLabel] = createSignal('evolution')
+
+export const [regionFilter, setRegionFilter] = createSignal()
+export const [dateFilter,   setDateFilter]   = createSignal()
