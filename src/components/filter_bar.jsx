4c4dc302c1c26231874af5add0d046936b3bffba
lucas.lvn <lucas.lvn97439@gmail.com>
2023-04-22 09:23:09 +0200

    Ajustemet du style

--- src/components/filter_bar.jsx
+++ src/components/filter_bar.jsx
@@ -13,15 +13,15 @@ export default function FilterBar() {
     }
 
     return (<>
-        <div id="filter-bar" class="w-full flex"> 
+        <div id="filter-bar" class="flex flex-wrap w-full rounded px-5 py-2 mb-5 bg-[#4d576c] text-white"> 
             <h5 class="w-1/6 mr-5">Filtres: </h5>
 
-            <section class="btn-group flex justify-start">
-                <select name="filter-region" onchange={handleRegionFilter}>
+            <section class="btn-group flex justify-start gap-3 items-center text-xs">
+                <select class="rounded p-1 px-3" name="filter-region" onchange={handleRegionFilter}>
                     <option value="all">Toutes</option>
                 </select>
 
-                <select name="date-filter" onchange={handleDateFilter}>
+                <select class="rounded p-1 px-3" name="date-filter" onchange={handleDateFilter}>
                     <option value="today">aujourd'hui</option>
                 </select>
             </section>
