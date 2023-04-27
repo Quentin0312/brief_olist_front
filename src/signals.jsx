4d4321b7d36cc735e9d5585d3adc764ca106cb94
lucas.lvn <lucas.lvn97439@gmail.com>
2023-04-27 18:07:40 +0200

    mise en place des signaux topographie & onRegion

--- src/signals.jsx
+++ src/signals.jsx
@@ -1,7 +1,9 @@
 import { createSignal } from "solid-js";
 
+export const [topographie, setTopographie] = createSignal()
 export const [map, setMap] = createSignal()
 export const [onLabel, setOnLabel] = createSignal('evolution')
+export const [onRegion, setOnRegion] = createSignal()
 
 export const [regions, setRegions] = createSignal([])
 export const [regionFilter, setRegionFilter] = createSignal()
