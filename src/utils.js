d9caf2e6670892bad83ee796818e6aa7a110545b
lucas.lvn <lucas.lvn97439@gmail.com>
2023-05-01 12:23:08 +0200

    ajout d'unne function pour récuperer le nom d'une région via l'objet topo

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,10 @@ export function findinTopoByName(topo_name){
     const topo_geometries = topographie().objects.default.geometries
     return topo_geometries.filter(el => el.properties['woe-name'] == topo_name)[0]
 }
+
+export function getNameInToto(topo_region){
+    if(topo_region.properties){
+        return topo_region.properties['woe-name']
+    }
+    return topo_region
+}
